cf43ff90892cf7ceb9d5254c5323adaad1a8a216
Nirmalaashok <148657928+Nirmalaashok@users.noreply.github.com>
2023-11-30 11:28:15 +0100

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,59 @@
+console.log("Hello World!")
+//Examples
+console.log("Halo, dunia!")// Indonesian
+console.log("Ciao, mondo!") // Italian
+console.log("Hola, mundo!") // Spanish
+console.log('Im awesome');
+console.log('Im awesome');
+// TODO -> here you initialize your variable
+console.log("the value of my variable x will be: 200");
+//modify the Variable number
+let G=5;
+console.log(G);
+G=20
+//round the number
+let z= 7.25;
+console.log("The round value of Z:"+Math.round(z)); 
+//Array
+let animals=['elephant', 'Cat', 'Dog', 'Tiger', 'Lion'];
+console.log(animals[3]);
+//check the length of String
+     let stringNam = "Nirmala Ashok";
+    let lengthOfName =stringNam.length;
+    console.log("Length of the Name:" +lengthOfName)
+    //checkSametype Variable
+let var1 = 20;
+let var2 = 100;
+function checkSameType(var1, var2) {
+  if (typeof var1 === typeof var2) {
+    console.log("SAME");
+  } else {
+    console.log("Different");
+  }
+}
+checkSameType(var1, var2);
+
+let x = 7;
+x = x % 3;
+
+console.log(x);
+let multipleArray = [42, "Hello", true, 3.14,];
+
+// Mutliple Arrays
+console.log("Array:", multipleArray);
+console.log("Element at index 0:", multipleArray[0]);  // Num
+console.log("Element at index 1:", multipleArray[1]);  // Str
+console.log("Element at index 2:", multipleArray[2]);  // Boolean
+console.log("Element at index 3:", multipleArray[3]);  // Num
+
+// Modifying 
+multipleArray[1] = "World";
+console.log("Modified array:",multipleArray[1]);
+//infinity
+const infinity1 = 6/0;
+const infinity2 = 10/0;
+
+console.log(infinity1 === infinity2); // true
+
+
+
